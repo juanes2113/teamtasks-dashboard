db86093968244fb054f8ac978e3617bb558c1d4b
juanes2113 <136110782+juanes2113@users.noreply.github.com>
2026-01-29 07:38:13 -0500

    Cors

--- TeamTasksBackend/TeamTasksDashboard/1. TeamTasks.API/Program.cs
+++ TeamTasksBackend/TeamTasksDashboard/1. TeamTasks.API/Program.cs
@@ -13,6 +13,17 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +36,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowAngular");
 
 app.MapControllers();
 
